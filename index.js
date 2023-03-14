b953730aaa5abad7c774af57dd0924b063888fdf
louispallett <louis.pallett@outlook.com>
2023-03-14 14:16:39 +0000

    JS created grid, not working properly yet.

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-const gridContainer = document.querySelector(".grid-container");
-const grid = document.createElement("div");
+// function that builds a grid in the "gridContainer"
+function createGrid(x) 
+{
+    let size = (x * x);
+    for (let rows = 0; rows < size; rows++) 
+    {
+        const gridContainer = document.querySelector("#grid-container");
+        const gridItem = document.createElement("div");
+        gridItem.classList.toggle(("grid-item"));
+        for (let columns = 0; columns < size; columns++) 
+        {
+            gridContainer.appendChild(gridItem);
+        }
+        gridItem.setAttribute("style", `width: ${960/(x * 2)}px; height: ${960/(x * 2)}px`);
+    }
+};
 
 
+createGrid(8);
+
+   
